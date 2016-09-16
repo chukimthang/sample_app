400129bcd8c506e1617d0bb59d368dbf2ff6948f
chukimthang94 <chukimthang94@gmail.com>
2016-09-16 15:51:52 +0700

    Finish user microposts

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,6 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
-
+User.create!(name: "Chu Kim Thang", email: "chukimthang94@gmail.com",
+  password: "123456", password_confirmation: "123456", admin: true,
+  activated: true, activated_at: Time.zone.now)
 
 40.times do |n|
   name = "user#{n}"
@@ -16,4 +11,10 @@
   User.create name: name, email:email, password: password,
     password_confirmation: password_confirmation,
     activated: true, activated_at: Time.zone.now
+end
+
+users = User.order(:created_at).take 6
+50.times do
+  content = Faker::Lorem.sentence 5
+  users.each {|user| user.microposts.create! content: content}
 end
